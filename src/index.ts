3a801d3d3e3870643117564e85b2747e503d69b2
1101hiroki_n <1101hiroki.n@gmail.com>
2024-07-21 21:04:58 +0900

    feat: expose api.

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,1 @@
-export { default as App } from "./App";
-export const hello: string = "Hello, World!";
-export const world: string = "World, Hello!";
-export const helloWorld = () => `${hello} ${world}!`;
+export * from "./api";
