2d6b0a892aeba5a6d27cb062cc133363ed5bb7c0
1101hiroki_n <1101hiroki.n@gmail.com>
2024-07-21 18:45:16 +0900

    feat: set up library mode.

---
+++ src/index.ts
@@ -0,0 +1,4 @@
+export { default as App } from "./App";
+export const hello: string = "Hello, World!";
+export const world: string = "World, Hello!";
+export const helloWorld = () => `${hello} ${world}!`;
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,25 @@
+import { resolve } from "path";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
+    build: {
+        lib: {
+            entry: resolve(__dirname, "src/index.ts"),
+            name: "Squama UI",
+            fileName: "index",
+        },
+        sourcemap: false,
+        emptyOutDir: false,
+        rollupOptions: {
+            external: ["react", "react-dom"],
+            output: {
+                globals: {
+                    react: "React",
+                    "react-dom": "ReactDOM",
+                },
+            },
+        },
+    },
 });
