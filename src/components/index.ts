1b6e3091eddcaac21369a90e54e27a113b96ba3c
1101hiroki_n <1101hiroki.n@gmail.com>
2024-09-07 20:28:33 +0900

    fix: Some components fixed.

--- src/components/index.ts
+++ src/components/index.ts
@@ -13,6 +13,5 @@ export * from "./ContextMenu/ContextMenu";
 export * from "./Tooltip/Tooltip";
 export * from "./Alert/Alert";
 export * from "./Badge/Badge";
-export * from "./Slider/Slider";
 export * from "./CircularLoader/CircularLoader";
 export * from "./SkeletonLoader/SkeletonLoader";
