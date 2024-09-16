c79682110849dccce150eb457f672369cf9fb34b
1101hiroki_n <1101hiroki.n@gmail.com>
2024-09-16 23:17:30 +0900

    fix(memo): use memo for Button component.

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React, { forwardRef, useMemo } from "react";
+import React, { forwardRef, memo, useMemo } from "react";
 import {
     ComponentSize,
     Elevation,
@@ -48,178 +48,180 @@ type ButtonProps = Modify<
     }
 >;
 
-export const Button = forwardRef<HTMLElement, ButtonProps>((props, ref) => {
-    const {
-        elevation = 0,
-        variant = "filled",
-        shape = "rounded",
-        color = "#000",
-        size = "m",
-        fitParentHeight = false,
+export const Button = memo(
+    forwardRef<HTMLElement, ButtonProps>((props, ref) => {
+        const {
+            elevation = 0,
+            variant = "filled",
+            shape = "rounded",
+            color = "#000",
+            size = "m",
+            fitParentHeight = false,
 
-        element = "auto",
-        href,
-        target,
-        rel,
-        onClick,
-        onClickInLoading,
-        type = "button",
+            element = "auto",
+            href,
+            target,
+            rel,
+            onClick,
+            onClickInLoading,
+            type = "button",
 
-        children,
-        disabled,
-        block,
-        loading = false,
+            children,
+            disabled,
+            block,
+            loading = false,
 
-        ...rest
-    } = props;
+            ...rest
+        } = props;
 
-    const context = useSquamaContext();
-    const theme = context.getCurrentTheme();
+        const context = useSquamaContext();
+        const theme = useMemo(() => context.getCurrentTheme(), [context]);
 
-    const cssVars = useMemo<React.CSSProperties>(() => {
-        const boxShadow = disabled
-            ? "none"
-            : getBoxShadowByElevation(elevation);
-        const borderRadius = getBorderRadiusByShape(shape ?? theme.shape);
-        const componentStyle = getComponentColor(theme, color, variant);
-        const height = getPixelFromComponentSize(size);
+        const cssVars = useMemo<React.CSSProperties>(() => {
+            const boxShadow = disabled
+                ? "none"
+                : getBoxShadowByElevation(elevation);
+            const borderRadius = getBorderRadiusByShape(shape ?? theme.shape);
+            const componentStyle = getComponentColor(theme, color, variant);
+            const height = getPixelFromComponentSize(size);
 
-        return {
-            "--s-button--height": height,
-            "--s-button--box-shadow": boxShadow,
-            "--s-button--border-radius": borderRadius,
+            return {
+                "--s-button--height": height,
+                "--s-button--box-shadow": boxShadow,
+                "--s-button--border-radius": borderRadius,
 
-            // Normal style
-            "--s-button--background-color--normal":
-                componentStyle.normal.background,
-            "--s-button--border--normal": componentStyle.normal.border,
-            "--s-button--color--normal": componentStyle.normal.text,
+                // Normal style
+                "--s-button--background-color--normal":
+                    componentStyle.normal.background,
+                "--s-button--border--normal": componentStyle.normal.border,
+                "--s-button--color--normal": componentStyle.normal.text,
 
-            // hover style (and focused style)
-            "--s-button--background-color--hover":
-                componentStyle.hover.background,
-            "--s-button--border--hover": componentStyle.hover.border,
-            "--s-button--color--hover": componentStyle.hover.text,
+                // hover style (and focused style)
+                "--s-button--background-color--hover":
+                    componentStyle.hover.background,
+                "--s-button--border--hover": componentStyle.hover.border,
+                "--s-button--color--hover": componentStyle.hover.text,
 
-            // active style
-            "--s-button--background-color--active":
-                componentStyle.active.background,
-            "--s-button--border--active": componentStyle.active.border,
-            "--s-button--color--active": componentStyle.active.text,
+                // active style
+                "--s-button--background-color--active":
+                    componentStyle.active.background,
+                "--s-button--border--active": componentStyle.active.border,
+                "--s-button--color--active": componentStyle.active.text,
 
-            // disabled style
-            "--s-button--background-color--disabled":
-                componentStyle.disabled.background,
-            "--s-button--border--disabled": componentStyle.disabled.border,
-            "--s-button--color--disabled": componentStyle.disabled.text,
-        } as React.CSSProperties;
-    }, [color, variant, shape, elevation, disabled]);
+                // disabled style
+                "--s-button--background-color--disabled":
+                    componentStyle.disabled.background,
+                "--s-button--border--disabled": componentStyle.disabled.border,
+                "--s-button--color--disabled": componentStyle.disabled.text,
+            } as React.CSSProperties;
+        }, [color, variant, shape, elevation, disabled]);
 
-    const isOnLoadingProcess =
-        loading === true || loading === "loading" || loading === "paused";
+        const isOnLoadingProcess =
+            loading === true || loading === "loading" || loading === "paused";
 
-    const innerNode = (
-        <>
-            <div className={styles.mainLayer}>
-                <div className={styles.childTextContainer}>
-                    <Text
-                        typeScale="button"
-                        className={styles.childText}
-                        element="div"
-                    >
-                        {children}
-                    </Text>
+        const innerNode = (
+            <>
+                <div className={styles.mainLayer}>
+                    <div className={styles.childTextContainer}>
+                        <Text
+                            typeScale="button"
+                            className={styles.childText}
+                            element="div"
+                        >
+                            {children}
+                        </Text>
+                    </div>
                 </div>
-            </div>
-            <div
-                className={buildClassName(
-                    styles.circularLoaderContainer,
-                    styles.absLayer,
-                )}
-            >
-                <CircularLoader
-                    loading={loading}
-                    size="1rem"
-                    className={styles.circularLoader}
-                />
-            </div>
-        </>
-    );
+                <div
+                    className={buildClassName(
+                        styles.circularLoaderContainer,
+                        styles.absLayer,
+                    )}
+                >
+                    <CircularLoader
+                        loading={loading}
+                        size="1rem"
+                        className={styles.circularLoader}
+                    />
+                </div>
+            </>
+        );
 
-    const elm = element === "auto" ? (href ? "a" : "button") : element;
+        const elm = element === "auto" ? (href ? "a" : "button") : element;
 
-    if (elm === "a") {
-        return (
-            <a
-                ref={ref as any}
-                {...rest}
-                href={href}
-                target={target}
-                rel={rel}
-                aria-disabled={disabled}
-                style={{
-                    ...rest.style,
-                    ...cssVars,
-                }}
-                className={buildClassName(
-                    styles.Button,
-                    rest.className,
-                    block && styles.block,
-                    isOnLoadingProcess && styles.isOnLoadingProcess,
-                    fitParentHeight && styles.fitParentHeight,
-                    squamaComponentClass,
-                )}
-            >
-                {innerNode}
-            </a>
-        );
-    } else if (elm === "button") {
-        return (
-            <button
-                ref={ref as any}
-                {...rest}
-                type={type}
-                disabled={disabled}
-                style={{
-                    ...rest.style,
-                    ...cssVars,
-                }}
-                className={buildClassName(
-                    styles.Button,
-                    rest.className,
-                    block && styles.block,
-                    isOnLoadingProcess && styles.isOnLoadingProcess,
-                    fitParentHeight && styles.fitParentHeight,
-                    squamaComponentClass,
-                )}
-                onClick={isOnLoadingProcess ? onClickInLoading : onClick}
-            >
-                {innerNode}{" "}
-            </button>
-        );
-    } else {
-        return (
-            <div
-                ref={ref as any}
-                {...rest}
-                aria-disabled={disabled}
-                role={href ? "link" : onClick ? "button" : undefined}
-                style={{
-                    ...rest.style,
-                    ...cssVars,
-                }}
-                className={buildClassName(
-                    styles.Button,
-                    rest.className,
-                    block && styles.block,
-                    isOnLoadingProcess && styles.isOnLoadingProcess,
-                    fitParentHeight && styles.fitParentHeight,
-                    squamaComponentClass,
-                )}
-                onClick={isOnLoadingProcess ? onClickInLoading : onClick}
-            >
-                {innerNode}{" "}
-            </div>
-        );
-    }
-});
+        if (elm === "a") {
+            return (
+                <a
+                    ref={ref as any}
+                    {...rest}
+                    href={href}
+                    target={target}
+                    rel={rel}
+                    aria-disabled={disabled}
+                    style={{
+                        ...rest.style,
+                        ...cssVars,
+                    }}
+                    className={buildClassName(
+                        styles.Button,
+                        rest.className,
+                        block && styles.block,
+                        isOnLoadingProcess && styles.isOnLoadingProcess,
+                        fitParentHeight && styles.fitParentHeight,
+                        squamaComponentClass,
+                    )}
+                >
+                    {innerNode}
+                </a>
+            );
+        } else if (elm === "button") {
+            return (
+                <button
+                    ref={ref as any}
+                    {...rest}
+                    type={type}
+                    disabled={disabled}
+                    style={{
+                        ...rest.style,
+                        ...cssVars,
+                    }}
+                    className={buildClassName(
+                        styles.Button,
+                        rest.className,
+                        block && styles.block,
+                        isOnLoadingProcess && styles.isOnLoadingProcess,
+                        fitParentHeight && styles.fitParentHeight,
+                        squamaComponentClass,
+                    )}
+                    onClick={isOnLoadingProcess ? onClickInLoading : onClick}
+                >
+                    {innerNode}{" "}
+                </button>
+            );
+        } else {
+            return (
+                <div
+                    ref={ref as any}
+                    {...rest}
+                    aria-disabled={disabled}
+                    role={href ? "link" : onClick ? "button" : undefined}
+                    style={{
+                        ...rest.style,
+                        ...cssVars,
+                    }}
+                    className={buildClassName(
+                        styles.Button,
+                        rest.className,
+                        block && styles.block,
+                        isOnLoadingProcess && styles.isOnLoadingProcess,
+                        fitParentHeight && styles.fitParentHeight,
+                        squamaComponentClass,
+                    )}
+                    onClick={isOnLoadingProcess ? onClickInLoading : onClick}
+                >
+                    {innerNode}{" "}
+                </div>
+            );
+        }
+    }),
+);
